cc7063377ea8b1da362c01738fe99d1856c610c5
Andre Luiz <andre.luiz_1985@hotmail.com>
2014-11-10 10:21:17 -0200

    criação das tabelas

--- myke/ConectaBanco.php
+++ myke/ConectaBanco.php
@@ -20,6 +20,19 @@ $dbPassword = "";
   	}else{
   		echo "DEU MERDA";
   	}
+    $sql="CREATE TABLE IF NOT EXISTS cadastro(
+  idnome varchar(40) NOT NULL ,
+  idemail varchar(50) NOT NULL,
+  idcpf int NOT NULL,
+  idlogin varchar(32) NOT NULL,
+  idsenha varchar(32) NOT NULL,
+  idconfsenha varchar(32) NOT NULL,
+  PRIMARY KEY(idlogin)
+   )";
+
+$res =  mysqli_query($servidordb,$sql);
+    
+
    mysqli_close($servidordb);
    //http://localhost/phpmyadmin/index.php?db=seila&token=9f811f9406d31c6569caa0f259802695;
 }

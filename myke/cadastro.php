53cf2580f4e2c1d571345b24e2e887ca1a75eb9f
nme <sds@yahoo.com.br>
2014-11-14 11:35:58 -0200

    Nada muito produtivo

--- myke/cadastro.php
+++ myke/cadastro.php
@@ -35,61 +35,81 @@
 </div><!-- /navbar wrapper -->
 
 	<!-- FORMULARIO DE CADASTRO   -->
-<div class= "container"
-<form class="form-horizontal" role="form" method="POST" action="">
-  <div class="form-group">
-    <label for="idnome" class="col-sm-2 control-label">Nome Completo</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="idnome" placeholder="Myke Wylian Dal'agnoll">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="idtelefone" class="col-sm-2 control-label">Telefone</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="idtelefone" placeholder="(XX)XXXX-XXXX">
-    </div>
-  </div>
-<div class="form-group">
-    <label for="idemail" class="col-sm-2 control-label">Email</label>
-    <div class="col-sm-10">
-    <div class="input-group">
-     <div class="input-group-addon">@</div>
-      <input type="email" class="form-control" id="idemail" placeholder="Myke@bol.com">
-    </div>
-    </div>
-  </div>
+<div class= "container">
+  <div class="jumbotron">
+    <form class="form-horizontal" role="form" method="POST" action="cadastra.php">
+      <div class="form-group">
+       <label for="idnome" class="col-sm-2 control-label">Nome Completo</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <input type="text" class="form-control"  id="idnome" placeholder="Myke Wylian Dal'agnol">
+          </div>
+        </div>
+      </div>
 
-<div class="form-group">
-    <label for="idcpf" class="col-sm-2 control-label">CPF</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="idcpf" placeholder="XXX.XXX.XXX-XX">
-    </div>
-  </div>  
+      <div class="form-group">
+        <label for="idtelefone" class="col-sm-2 control-label">Telefone</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="idtelefone" placeholder="(XX)XXXX-XXXX">
+          </div>
+        </div>
+      </div>
 
-  <div class="form-group">
-    <label for="idlogin" class="col-sm-2 control-label">Login</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="idlogin" placeholder="mykewd">
-    </div>
-  </div>  
+      <div class="form-group">
+        <label for="idemail" class="col-sm-2 control-label">Email</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <div class="input-group">
+              <div class="input-group-addon">@</div>
+              <input type="email" class="form-control" id="idemail" placeholder="Myke@bol.com">
+            </div>
+          </div>
+        </div>
+      </div>
 
-  <div class="form-group">
-    <label for="idsenha" class="col-sm-2 control-label">Senha</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="idsenha" placeholder="5u453Nh4">
-    </div>
+      <div class="form-group">
+        <label for="idcpf" class="col-sm-2 control-label">CPF</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="idcpf" placeholder="XXX.XXX.XXX-XX">
+          </div>
+        </div>
+      </div> 
+
+      <div class="form-group">
+        <label for="idlogin" class="col-sm-2 control-label">Login</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="idlogin" placeholder="mykewd">
+          </div>
+        </div>
+      </div>  
+
+      <div class="form-group">
+        <label for="idsenha" class="col-sm-2 control-label">Senha</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="idsenha" placeholder="5u453Nh4">
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="idconfsenha" class="col-sm-2 control-label">Confirme sua Senha</label>
+        <div class="controls">
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="idconfsenha" placeholder="5u453Nh4">
+          </div>
+        </div>
+      </div>
+
+      <center>
+        <button type="submit" class="btn btn-default">Enviar</button>
+      </center>
+
+    </form>
   </div>
-
-  <div class="form-group">
-    <label for="idconfsenha" class="col-sm-2 control-label">Confirme sua Senha</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="idconfsenha" placeholder="5u453Nh4">
-    </div>
-  </div><center>
-  <button type="submit" class="btn btn-default">Enviar</button>
-  </center>
-  </form>
 </div>
-
  </body> 
 </html>
